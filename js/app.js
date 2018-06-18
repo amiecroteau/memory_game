3ad959d544e16e3f412f8baa07443d8d3624adbb
Amie Croteau <amiecroteau@gmail.com>
2018-06-18 15:51:37 -0400

    Added timer

--- js/app.js
+++ js/app.js
@@ -20,10 +20,40 @@ let modal = document.getElementById("end-modal");
 let modalText = document.getElementById("end-text");
 const newGameBtn = document.getElementById("newGame");
 const endGameBtn = document.getElementById("closeGame");
-let timer = document.querySelector(".timer");
-let seconds = 0;
+//let timer = document.querySelector(".timer");
+//let seconds = 0;
 let running;
 
+//followed fellow fender on slack Chris N for his timer post
+let sec = 0;
+let min = 0;
+let timer;
+
+window.addEventListener("load", function startTimer() {
+timer = setInterval(insertTime, 1500);
+});
+
+function stopTimer() {
+clearInterval(timer);
+sec = 0;
+min = 0;
+}
+
+function insertTime() {
+sec++;
+
+if (sec < 10) {
+sec = `0${sec}`;
+}
+
+if (sec >= 60) {
+min++;
+sec = "00";
+}
+
+// display time
+document.querySelector('.timer-output').innerHTML = "0" + min + ":" + sec;
+}
 //refresh game
 const refresh = document.querySelector('.restart');
 refresh.addEventListener('click', function restart() {
@@ -31,9 +61,11 @@ refresh.addEventListener('click', function restart() {
 });
 
 
+
 //referenced this shuffle function by https://css-tricks.com/snippets/javascript/shuffle-array/ as the provided
 //function was not working for me and providing an error for array.length
 //Shuffle cards
+
 function Shuffle(o) {
 	for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
 	return o;
@@ -41,7 +73,6 @@ function Shuffle(o) {
 cards.sort(function () {
 	return 0.25 - Math.random()
 });
-
 
 /*
 function shuffle(array) {
@@ -147,41 +178,39 @@ allCards.forEach(function (card) {
 //Thanks to fellow Fender Matthew Cranford's walkthrough, I utilized style.display property and his for of for LOOP to check the moves to star ration.https://matthewcranford.com/memory-game-walkthrough-part-5-moves-stars/
 
 let stars = document.querySelectorAll('.stars li');
-let info = document.querySelectorAll('.fa fa-star');
+
 let starCount = 3;
 
 function starCounter() {
 
 	for (let a = 0; a < 20; a++) {
-		if (moves > 10) {
+		if (moves > 20) {
 
 			for (star of stars) {
 				stars[2].style.display = 'none';
 				//stars[2].style.display = 'none';
-				starCount=0;
+				starCount = 0;
 			}
 
-		} else if (moves > 8) {
+		} else if (moves > 15) {
 
 			for (star of stars) {
 				stars[1].style.display = 'none';
 				starCount = 1;
 			}
-		} else if (moves > 5) {
+		} else if (moves > 10) {
 			for (star of stars) {
 				stars[0].style.display = 'none';
 				starCount = 2;
-				
+
 			}
 		}
 	}
 }
 
 
-
-
-
 //once all matches are found a modal opens up and the timer stops
+
 
 
 function gameOver() {
@@ -190,18 +219,20 @@ function gameOver() {
 		modal.style.display = "block";
 		if (starCount >= 2) {
 
-			modalText.innerText = `Congratulations! You finished the game in ${moves} moves in ${seconds} seconds. You've earned ${starCount} stars! What would you like to do next?`;
+			modalText.innerText = `Congratulations! You finished the game in ${moves} moves in ${min} minutes and ${sec} seconds. You've earned ${starCount} stars! What would you like to do next?`;
 		} else {
 
-			modalText.innerText = `Congratulations! You finished the game in ${moves} moves in ${seconds} seconds. You've earned ${starCount} star! What would you like to do next?`;
+			modalText.innerText = `Congratulations! You finished the game in ${moves} moves in ${min} minutes and ${sec} seconds. You've earned ${starCount} star! What would you like to do next?`;
 
 		}
+
 	}
 }
 
+
+
 newGameBtn.addEventListener("click", function () {
 	modal.style.display = "none";
-
 	window.location.reload(false);
 
 });
@@ -211,4 +242,4 @@ endGameBtn.addEventListener("click", function () {
 	modal.style.display = "none";
 	window.close();
 
-})
+});
